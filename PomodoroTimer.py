a6729c14f5183d9b1d45495fd8465034ddd8aa96
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-11-19 13:17:46 +0800

    The start button's function has been defined and the countdown timer works.

--- PomodoroTimer.py
+++ PomodoroTimer.py
@@ -33,7 +33,7 @@ class PomodoroTimer:
         self.pomodoro_timer_label = ttk.Label(self.tab1, text="25:00", font=("Ubuntu", 48))
         self.pomodoro_timer_label.pack(pady=20)
 
-        self.short_break_timer_label = ttk.Label(self.tab2, text="5:00", font=("Ubuntu", 48))
+        self.short_break_timer_label = ttk.Label(self.tab2, text="05:00", font=("Ubuntu", 48))
         self.short_break_timer_label.pack(pady=20)
 
         self.long_break_timer_label = ttk.Label(self.tab3, text="15:00", font=("Ubuntu", 48))
@@ -57,7 +57,7 @@ class PomodoroTimer:
         self.reset_button.grid(row=0, column=2)
 
         self.pomodoro_counter_label = ttk.Label(self.grid_layout, text="Pomodoro: 0", font=("Ubuntu", 16))
-        self.pomodoro_counter_label.grid(row=1, column=0, columnspan=3)
+        self.pomodoro_counter_label.grid(row=1, column=0, columnspan=3, pady=10)
 
         self.pomodoros = 0
         self.skipped = False
@@ -65,11 +65,62 @@ class PomodoroTimer:
 
         self.root.mainloop()
 
-    def start_timer_thread(self):
-        pass
 
+    def start_timer_thread(self):
+        t = threading.Thread(target=self.start_timer)
+        t.start()
+
+#define the start_timer fuunction
     def start_timer(self):
-        pass
+        self.stopped = False
+        self.skipped = False
+        timer_id = self.tabs.index(self.tabs.select()) + 1
+
+        if timer_id == 1:
+            full_seconds = 60 * 25
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.pomodoro_timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds -=1
+
+            if not self.stopped or self.skipped:
+                self.pomodoros += 1
+                self.pomodoro_counter_label.config(text=f"Pomodoro: {self.pomodoros}")
+                if self.pomodoros % 4 == 0:
+                    self.tabs.select(2)
+                    self.start_timer()
+                else:
+                    self.tabs.select(1)
+                self.start_timer
+
+        elif timer_id == 2:
+            full_seconds = 60 * 5
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.short_break_timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds -=1
+
+            if not self.stopped or self.skipped:
+                self.tabs.select(0)
+                self.start_timer()
+
+        elif timer_id == 3:
+            full_seconds = 60 * 15
+            while full_seconds > 0 and not self.stopped:
+                minutes, seconds = divmod(full_seconds, 60)
+                self.long_break_timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.root.update()
+                time.sleep(1)
+                full_seconds -=1
+
+            if not self.stopped or self.skipped:
+                self.tabs.select(0)
+                self.start_timer()
+
 
     def reset_clock(self):
         pass
