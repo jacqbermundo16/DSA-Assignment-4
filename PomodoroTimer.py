77f6f768889341231e8453042194815341770e3b
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2022-11-19 11:36:38 +0800

    Creating tabs for Pomodoro, Short Break, and Long Break.

--- PomodoroTimer.py
+++ PomodoroTimer.py
@@ -15,7 +15,27 @@ class PomodoroTimer:
         self.root = tk.Tk()
         self.root.geometry("600x300")
         self.root.title("Pomodoro Timer")
+
+    #customize the content
+        self.s = ttk.Style()
+        self.s.configure("TNotebook.Tab", font=("Ubuntu", 16))
+        self.s.configure("TButton", font=("Ubuntu", 16))
+
+        self.tabs = ttk.Notebook(self.root)
+        self.tabs.pack(fill="both", pady=10, expand=True)
+
+    #creating tabs for the timer
+        self.tab1 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2 = ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3 = ttk.Frame(self.tabs, width=600, height=100)
+    
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Short Break")
+        self.tabs.add(self.tab3, text="Long Break")
+
         self.root.mainloop()
-        
+     
+
+
 #call to check if it works
 PomodoroTimer()
